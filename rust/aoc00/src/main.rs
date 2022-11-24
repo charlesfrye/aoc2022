a4ba943457066f7f37d8d0605e0722bdf7af8008
Charles Frye <cfrye59@gmail.com>
2022-11-24 03:57:45 +0000

    better starting project structure for rust

--- rust/aoc00/src/main.rs
+++ rust/aoc00/src/main.rs
@@ -1,8 +1,31 @@
+use std::env;
+use std::fs;
+
+
 fn main() {
-   println!("Hello, world!");
+  // get args from command line
+  let args: Vec<String> = env::args().collect();
+  let filename: &str = parse_args(&args);
+
+  let contents = get_contents(filename);
+
+  println!("{}", contents);
+}
+
+fn parse_args(args: &[String]) -> &str {
+  let filename: &String = &args[1];
+
+  filename
+}
+
+fn get_contents(filename: &str) -> String {
+  // read file
+  let contents: String = fs::read_to_string(filename)
+        .expect(r#"Error reading file"#);
+  contents
 }
 
 #[test]
 fn test_main() {
-   main()
+  assert_eq!(get_contents("test.txt"), "hello, world!\n")
 }
